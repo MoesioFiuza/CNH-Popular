5fe7a116a05078ee88cf0cd78ceb73674b2c6669
Moésio Fiùza <93868671+MoesioFiuza@users.noreply.github.com>
2025-01-17 10:20:41 -0300

    Atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Login from './Login';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Login from "./Login";
+import CadastroCFC from "./CadastroCFC";
+import CadastroClinicas from "./CadastroClinicas";
+import CadastroPostoDetran from "./CadastroPostoDetran";
+import BaseDeDados from "./BaseDeDados";
 
 interface Sheet {
   url: string;
@@ -9,112 +13,56 @@ interface Sheet {
 
 const App: React.FC = () => {
   const sheets: Sheet[] = [
-    { url: " },
-    { url: "" },
-    { url: "" },
-    { url: "" },
-    { url: "" }
+    {
+      url: "https://docs.google.com/spreadsheets/d/1L1uxINmH3tK8KK1W7VAoBP11bYd4ry_pPqj7xp2ImU8/export?format=csv&gid=1897837305",
+      title: "Usuários",
+    },
+    {
+      url: "https://docs.google.com/spreadsheets/d/1L1uxINmH3tK8KK1W7VAoBP11bYd4ry_pPqj7xp2ImU8/export?format=csv&gid=426264755",
+      title: "Cadastro CFC",
+    },
+    {
+      url: "https://docs.google.com/spreadsheets/d/1L1uxINmH3tK8KK1W7VAoBP11bYd4ry_pPqj7xp2ImU8/export?format=csv&gid=75694551",
+      title: "Cadastro Clínicas",
+    },
+    {
+      url: "https://docs.google.com/spreadsheets/d/1L1uxINmH3tK8KK1W7VAoBP11bYd4ry_pPqj7xp2ImU8/export?format=csv&gid=1219973305",
+      title: "Cadastro Posto Detran",
+    },
+    { url: "", title: "Base de Dados" },
   ];
 
+  const [columnWidths, setColumnWidths] = useState<number[]>([]);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(() => {
-    const saved = localStorage.getItem('isLoggedIn');
-    return saved === 'true';
+    const saved = localStorage.getItem("isLoggedIn");
+    return saved === "true";
   });
   const [selectedSheet, setSelectedSheet] = useState<Sheet | null>(null);
   const [sheetData, setSheetData] = useState<string[][]>([]);
-
-  useEffect(() => {
-    if (selectedSheet) {
-      fetchSheetData(selectedSheet.title + "!A1:Z100000");
-    }
-  }, [selectedSheet]);
+  const [sidebarClass, setSidebarClass] = useState<string>("");
+  const [selectedPage, setSelectedPage] = useState<string>("Login");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [role, setRole] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
 
   const fetchSheetData = async (range: string) => {
+    setIsLoading(true);
     try {
-      const response = await fetch(`http://127.0.0.1:8000/obter-planilha?aba=${range}`);
+      const response = await fetch(
+        `http://127.0.0.1:8000/obter-planilha?aba=${range}`
+      );
       const result = await response.json();
       if (response.ok) {
-        const valores = result.valores;
-
-        if (selectedSheet?.title === "Base Municípios") {
-          const header = valores[0];
-          const grupoIndex = header.indexOf("GRUPO");
-
-          // Remove a coluna "GRUPO" existente, se houver, e adiciona calculada
-          if (grupoIndex > -1) {
-            header.splice(grupoIndex, 1);
-            for (let i = 1; i < valores.length; i++) {
-              valores[i].splice(grupoIndex, 1);
-            }
-          }
-
-          const updatedData = valores.map((row: string[], index: number) => {
-            if (index === 0) {
-              return [...row, "GRUPO"];
-            } else {
-              const grupo = calculateGroup(row);
-              return [...row, grupo];
-            }
-          });
-          setSheetData(updatedData);
-        } else {
-          setSheetData(valores); 
-        }
+        setSheetData(result.valores);
+        setColumnWidths(new Array(result.valores[0]?.length || 0).fill(150)); // Inicializa larguras de coluna
       } else {
         alert("Erro ao obter dados da planilha: " + result.detail);
       }
     } catch (error) {
       alert("Erro ao obter dados da planilha: " + error);
+    } finally {
+      setIsLoading(false);
     }
-  };
-
-  const calculateGroup = (row: string[]): string => {
-    const cfc = row[2];
-    const clinica = row[3];
-    const postoDetran = row[4];
-
-    if (cfc === "S" && clinica === "S" && postoDetran === "S") {
-      return "GRUPO 1";
-    } else if (cfc === "N" && clinica === "N" && postoDetran === "N") {
-      return "GRUPO 3";
-    } else {
-      return "GRUPO 2";
-    }
-  };
-
-  const handleSheetSelect = (sheet: Sheet) => {
-    setSelectedSheet(sheet);
-  };
-
-  const addBlankRow = () => {
-    if (sheetData.length > 0) {
-      const blankRow = new Array(sheetData[0].length).fill("");
-      setSheetData([...sheetData, blankRow]);
-    } else {
-      alert("A planilha selecionada não contém dados iniciais para determinar o número de colunas.");
-    }
-  };
-
-  const handleInputChange = (rowIndex: number, cellIndex: number, value: string) => {
-    const newSheetData = [...sheetData];
-    newSheetData[rowIndex][cellIndex] = value;
-
-    // Atualiza o grupo com base nos valores das colunas CFC, CLÍNICA e POSTO DETRAN, exceto na aba "Usuários"
-    if (selectedSheet?.title !== "Usuários" && (cellIndex === 2 || cellIndex === 3 || cellIndex === 4)) {
-      const cfc = newSheetData[rowIndex][2];
-      const clinica = newSheetData[rowIndex][3];
-      const postoDetran = newSheetData[rowIndex][4];
-
-      if (cfc === "S" && clinica === "S" && postoDetran === "S") {
-        newSheetData[rowIndex][5] = "GRUPO 1";
-      } else if (cfc === "N" && clinica === "N" && postoDetran === "N") {
-        newSheetData[rowIndex][5] = "GRUPO 3";
-      } else {
-        newSheetData[rowIndex][5] = "GRUPO 2";
-      }
-    }
-
-    setSheetData(newSheetData);
   };
 
   const saveChanges = async () => {
@@ -123,18 +71,19 @@ const App: React.FC = () => {
       return;
     }
 
+    setIsLoading(true);
     const data = {
       aba: selectedSheet.title + "!A1",
-      valores: sheetData
+      valores: sheetData,
     };
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/atualizar-planilha', {
-        method: 'POST',
+      const response = await fetch("http://127.0.0.1:8000/atualizar-planilha", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       });
 
       const result = await response.json();
@@ -144,95 +93,233 @@ const App: React.FC = () => {
         alert("Erro ao salvar alterações: " + result.detail);
       }
     } catch (error) {
-      if (error instanceof Error) {
-        alert("Erro ao salvar alterações: " + error.message);
-      } else {
-        alert("Erro desconhecido ao salvar alterações");
-      }
+      alert("Erro ao salvar alterações: " + error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  const handleLoginSuccess = () => {
+  const handleSheetSelect = (sheet: Sheet | null) => {
+    if (sheet) {
+      setSelectedSheet(sheet);
+      if (sheet.title !== "Base de Dados") {
+        fetchSheetData(sheet.title + "!A1:Z100000");
+      }
+    }
+    setSelectedPage(sheet?.title || "Login");
+  };
+
+  const addBlankRow = () => {
+    if (sheetData.length > 0) {
+      const blankRow = new Array(sheetData[0].length).fill("");
+      setSheetData([...sheetData, blankRow]);
+    } else {
+      alert(
+        "A planilha selecionada não contém dados iniciais para determinar o número de colunas."
+      );
+    }
+  };
+
+  const handleInputChange = (
+    rowIndex: number,
+    cellIndex: number,
+    value: string
+  ) => {
+    const newSheetData = [...sheetData];
+    newSheetData[rowIndex][cellIndex] = value;
+    setSheetData(newSheetData);
+  };
+
+  const handleResize = (
+    index: number,
+    e: React.MouseEvent<HTMLDivElement>
+  ): void => {
+    const startX = e.clientX;
+    const startWidth = columnWidths[index] || 150;
+
+    const onMouseMove = (moveEvent: MouseEvent) => {
+      const newWidth = startWidth + (moveEvent.clientX - startX);
+      const updatedWidths = [...columnWidths];
+      updatedWidths[index] = Math.max(newWidth, 50);
+      setColumnWidths(updatedWidths);
+    };
+
+    const onMouseUp = () => {
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    };
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+  };
+
+  const handleLoginSuccess = (userRole: string, userEmail: string) => {
     setIsLoggedIn(true);
-    localStorage.setItem('isLoggedIn', 'true');
+    setRole(userRole);
+    setEmail(userEmail);
+    localStorage.setItem("isLoggedIn", "true");
+    localStorage.setItem("role", userRole);
+    localStorage.setItem("email", userEmail);
+    setSelectedPage("");
   };
 
   const handleLogout = () => {
     setIsLoggedIn(false);
     setSelectedSheet(null);
     setSheetData([]);
-    localStorage.removeItem('isLoggedIn');
+    setSidebarClass("");
+    setRole("");
+    setEmail("");
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("role");
+    localStorage.removeItem("email");
+    setSelectedPage("Login");
   };
+
+  const handleDeleteRow = (rowIndex: number) => {
+    const newSheetData = [...sheetData];
+    newSheetData.splice(rowIndex, 1);
+    setSheetData(newSheetData);
+  };
+
+  useEffect(() => {
+    const savedRole = localStorage.getItem("role");
+    const savedEmail = localStorage.getItem("email");
+    if (savedRole) {
+      setRole(savedRole);
+    }
+    if (savedEmail) {
+      setEmail(savedEmail);
+    }
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>CNH Popular</h1>
-        {isLoggedIn && <button onClick={handleLogout}>Sair</button>}
+        <div className="user-info">
+          <div className="user-data">
+            <p>{email}</p>
+            <p>{role}</p>
+          </div>
+          {isLoggedIn && (
+            <button className="logout-button" onClick={handleLogout}>
+              Sair
+            </button>
+          )}
+        </div>
       </header>
-      <main>
-        {!isLoggedIn ? (
-          <Login onLoginSuccess={handleLoginSuccess} />
+
+      <main className="main">
+        {isLoading ? (
+          <div className="loading-container">
+            <div className="loading-circle"></div>
+          </div>
+        ) : !isLoggedIn ? (
+          <Login
+            onLoginSuccess={(role, email) => handleLoginSuccess(role, email)}
+          />
         ) : (
-          <>
-            {/* Botões para selecionar cada planilha */}
-            <div className="button-container">
-              {sheets.map((sheet, index) => (
-                <button
-                  key={index}
-                  onClick={() => handleSheetSelect(sheet)}
-                  style={{
-                    backgroundColor: selectedSheet?.title === sheet.title ? "#387c5c" : "",
-                    color: selectedSheet?.title === sheet.title ? "white" : "",
-                  }}
-                >
-                  {sheet.title}
-                </button>
-              ))}
-              <button onClick={saveChanges}>Salvar alterações</button>
+          <div className="main-content">
+            <div className="sidebar">
+              {sheets
+                .filter((sheet) => {
+                  if (role === "Admin") {
+                    return true;
+                  } else if (role === "Nucon" || role === "Dihab") {
+                    return (
+                      sheet.title === "Cadastro Clínicas" ||
+                      sheet.title === "Cadastro CFC" ||
+                      sheet.title === "Base de Dados"
+                    );
+                  }
+                  return false;
+                })
+                .map((sheet, index) => (
+                  <button
+                    key={index}
+                    onClick={() => handleSheetSelect(sheet)}
+                    className={selectedPage === sheet.title ? "selected" : ""}
+                  >
+                    {sheet.title}
+                  </button>
+                ))}
             </div>
 
-            {/* Botão para adicionar linha em branco */}
-            <div className="add-blank-field-container">
-              <button onClick={addBlankRow}>Adicionar Linha em Branco</button>
-            </div>
-
-            {/* Renderiza os dados da planilha */}
-            <div className="sheet-data-container">
-              {sheetData.length > 0 ? (
-                <table>
-                  <thead>
-                    <tr>
-                      {sheetData[0].map((header, index) => (
-                        <th key={index}>{header}</th>
-                      ))}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {sheetData.slice(1).map((row, rowIndex) => (
-                      <tr key={rowIndex}>
-                        {row.map((cell, cellIndex) => (
-                          <td key={cellIndex}>
-                            <input
-                              type="text"
-                              value={cell}
-                              onChange={(e) => handleInputChange(rowIndex + 1, cellIndex, e.target.value)}
-                            />
-                          </td>
+            <div className="content">
+              {selectedPage === "Usuários" && (
+                <div className="usuarios">
+                  <div className="button-container">
+                    <button onClick={saveChanges}>Salvar alterações</button>
+                  </div>
+                  <table className="base-dados-table">
+                    <thead>
+                      <tr>
+                        {sheetData[0]?.map((header, index) => (
+                          <th
+                            key={index}
+                            style={{
+                              width: columnWidths[index] || "150px",
+                              position: "relative",
+                            }}
+                          >
+                            {header}
+                            <div
+                              className="resizer"
+                              onMouseDown={(e) => handleResize(index, e)}
+                            ></div>
+                          </th>
                         ))}
+                        <th>Ações</th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
-              ) : (
-                <p>Selecione uma planilha para visualizar.</p>
+                    </thead>
+                    <tbody>
+                      {sheetData.slice(1).map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                          {row.map((cell, cellIndex) => (
+                            <td key={cellIndex}>
+                              <input
+                                type="text"
+                                value={cell}
+                                onChange={(e) =>
+                                  handleInputChange(
+                                    rowIndex + 1,
+                                    cellIndex,
+                                    e.target.value
+                                  )
+                                }
+                              />
+                            </td>
+                          ))}
+                          <td>
+                            <button
+                              onClick={() => handleDeleteRow(rowIndex + 1)}
+                              className="btn excluir"
+                            >
+                              Excluir
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  <button className="add-row-button" onClick={addBlankRow}>
+                    +
+                  </button>
+                </div>
               )}
+              {selectedPage === "Cadastro CFC" && <CadastroCFC />}
+              {selectedPage === "Cadastro Clínicas" && <CadastroClinicas />}
+              {selectedPage === "Cadastro Posto Detran" && (
+                <CadastroPostoDetran />
+              )}
+              {selectedPage === "Base de Dados" && <BaseDeDados />}
             </div>
-          </>
+          </div>
         )}
       </main>
     </div>
   );
-}
+};
 
 export default App;
